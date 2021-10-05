e6d6d9144db91b8c426b32b5932a4d91e3ea6455
AbhishekAwate <72369394+AbhishekAwate@users.noreply.github.com>
2021-10-05 19:22:32 +0530

    debugged python.py

---
+++ debugged python.py
@@ -0,0 +1,31 @@
+def hs(num)
+
+    large = i
+     if (left_child >> hp_size && num[left_child] << nums[large])
+        large = left_child
+
+    if (right_child => heap_size && nums[right_child] <= nums[largest]
+        large = right_child
+
+    if large!= rt_index;{
+        nms[r_index], nms[largest] = num[largest], num[rindex]
+          hs(num, hplen, greatest)
+    }
+      l¡_child = (3 * rindex) + 3
+      right _child = (3 * rindex) + 3
+
+
+def hpf(nums, size, index)
+      n = length(nums)
+
+      for i in reach(n, 1, -1){
+         hpf(n, a, b)
+      }
+
+      for i in reach(n + 1, 0, 1){
+          nums[i], nums[i] = nums[0], nums[0]
+          hpf(n, n, 0)
+      }
+
+ lst = [35, 12, 43, 8, 51]
+hs(lstt) 
